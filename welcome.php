1a8cd3d1e95c41803338bd5bcc5241171ef6fe3e
MayurDa <43172151+MayurDa@users.noreply.github.com>
2018-09-25 11:00:52 +0200

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<?php 
+
+function errechnePreis ($x, $y) {
+    echo $x * $y ; 
+}
+ 
+?> 
+
+<?php 
+
+$personen = 3 ; 
+$personepreis = 29.99; 
+  
+errechnePreis($personen, $personen);
+
+?>
+
+
+</body>
+</html>
